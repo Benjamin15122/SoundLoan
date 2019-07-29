38ec55952830dac640c5045ff2fb91edf499e4b2
luowebb <151220068@smail.nju.edu.cn>
2019-07-29 12:45:58 +0800

    complete SelectUserType component

--- front-end/src/pages/user/components/SelectUserType/index.jsx
+++ front-end/src/pages/user/components/SelectUserType/index.jsx
@@ -1,8 +1,11 @@
 import React, {Component} from 'react'
 import style from './style.less'
-import { Button, Icon } from 'antd';
+import { Button, Card, Icon, Row, Col } from 'antd';
 import { Link } from 'umi';
+import enterpriseSvg from '../../asset/enterprise.svg';
+import personSvg from '../../asset/person.svg';
 
+const { Meta } = Card
 
 export default class SelectUserType extends Component {
 
@@ -17,20 +20,26 @@ export default class SelectUserType extends Component {
         return  <>
             <div className={style.title}>请选择您的身份</div>
             <div>
-                <Link to={linkToPerson}>
-                    <Button
-                        icon='user' shape='round' size='large'
-                    >
-                        个人用户
-                    </Button>
-                </Link>
-                <Link to={linkToEnterprise}>
-                    <Button
-                        icon='team' shape='round' size='large'
-                    >
-                        企业用户
-                    </Button>
-                </Link>
+                <Row gutter={32} className={style.pane} align='middle' justify='center' type='flex'>
+                    <Col span={4}>
+                        <Link to={linkToPerson}>
+                            <Card className={style.card} bordered hoverable
+                                cover={<img className={style.img} alt="personal" src={personSvg} />}
+                            >
+                                个人用户
+                            </Card>
+                        </Link>
+                    </Col>
+                    <Col span={4}>
+                        <Link to={linkToEnterprise}>
+                            <Card className={style.card} bordered hoverable
+                                cover={<img className={style.img} alt="enterprise" src={enterpriseSvg} />}
+                            >
+                                企业用户
+                            </Card>
+                        </Link>
+                    </Col>
+                </Row>
             </div>
         </>
         
