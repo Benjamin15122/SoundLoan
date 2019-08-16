e481e942eaf3a501e93f2ac674f00526db726c8a
luowebb <151220068@smail.nju.edu.cn>
2019-08-17 00:42:02 +0800

    update Introduction.jsx

--- front-end/src/pages/entInfo/details/components/Introduction.jsx
+++ front-end/src/pages/entInfo/details/components/Introduction.jsx
@@ -18,26 +18,33 @@ const itemLabelList = [
 export default class Introduction extends Component {
 
   static defaultProps = {
-    logoUrl: '',
+    logoUrl: 'https://os.alipayobjects.com/rmsportal/QBnOOoLaAfKPirc.png',
     descriptions: {
-
+     'name': '某某企业',
+     'foundation_date': '某年某月某日',
+     'corporate_capital': '很多',
+     'register_capital': '大量',
+     'legal_person_name': '某人',
+     'contact': '150xxxx9999',
+     'website': 'www.mm.com',
+     'address': '某省某市某某区',
+      'description': '企业介绍...',
     }
   };
 
   render() {
+    const {logoUrl, descriptions} = this.props;
     return <>
       <Row>
         <Col span={20}>
           <Descriptions column={2}>
             {itemLabelList.map((itemLabelPair) => {
-              return <DescItem label={itemLabelPair[1]}>{itemLabelPair[0]}</DescItem>
+              return <DescItem label={itemLabelPair[1]}>{descriptions[itemLabelPair[0]]}</DescItem>
             })}
           </Descriptions>
         </Col>
         <Col span={4}>
-          <Card cover={<img alt='logo'
-                            src='https://os.alipayobjects.com/rmsportal/QBnOOoLaAfKPirc.png'/>}
-          />
+          <Card cover={<img alt='logo' src={logoUrl}/>}/>
         </Col>
       </Row>
     </>
