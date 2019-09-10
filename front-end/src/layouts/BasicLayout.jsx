f1045f5904bf8a9290a0821ebb26ba505e067f77
luowebb <151220068@smail.nju.edu.cn>
2019-09-10 14:58:38 +0800

    update footer

--- front-end/src/layouts/BasicLayout.jsx
+++ front-end/src/layouts/BasicLayout.jsx
@@ -25,7 +25,7 @@ const menuDataRender = menuList =>
 const footerRender = (_, defaultDom) => {
   if (!isAntDesignPro()) {
     // return defaultDom;
-  return <footer style={{ textAlign: 'center', background: 'rgb(28,1,79)', color: 'white' }}>
+  return <footer style={{ textAlign: 'center', background: 'white' }}>
       <div>本产品暂不向在校学生提供服务</div>
       <div>如有问题，请联系xxxxxxx@xxx.com</div>
       <small>copyright&copy;LoanGuardians</small>
