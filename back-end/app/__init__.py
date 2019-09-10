6fdb0437d834cb0d568aa1c4388e766d75101358
mingxin <171250553@smail.nju.edu.cn>
2019-09-10 22:14:08 +0800

    ming

--- back-end/app/__init__.py
+++ back-end/app/__init__.py
@@ -24,7 +24,6 @@ from app.contract_analyze import routes
 from app.loan_recommendation import routes
 from app.authentication import routes
 from app.contract_assignment import routes
-from app.news_crawl import routes
 from models import IndividualUser, EnterpriseUser, LoanProduct, LoanRecord, Contract, LoanProductComment, EnterpriseNews
 
 #db.create_all()
