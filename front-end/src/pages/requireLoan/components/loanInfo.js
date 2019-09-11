0741d11456022d83fe78dad007e5d00fc286e528
7D24H <31737338+7D24H@users.noreply.github.com>
2019-09-11 20:28:00 +0800

    reconstruct loaninfo

--- front-end/src/pages/requireLoan/components/loanInfo.js
+++ front-end/src/pages/requireLoan/components/loanInfo.js
@@ -1,6 +1,9 @@
 import React, { Component } from 'react';
 import { Form, Input, DatePicker, TimePicker, Select, Cascader, InputNumber, Button } from 'antd';
 import center from '@/pages/personalManagement/center';
+import { createHashHistory } from 'history'
+
+const history = createHashHistory();
 
 const { Option } = Select;
 
@@ -15,14 +18,26 @@ const formItemLayout = {
   },
 };
 
-export class index extends Component {
+
+
+export class LoanInfo extends Component {
+
+    handleSubmit (e){
+        e.preventDefault();
+        //...
+        const path = `/enterInfo`
+        console.log("click")
+        history.push(path)
+    }
+
     render() {
         return (
             <div>
                 <h2>填写信息</h2>
-                  <Form {...formItemLayout}>
+                <div style={{marginLeft:'15%'}}>
+                    <Form {...formItemLayout}>
                         <Form.Item label="借款金额">
-                            <InputNumber placeholder="请输入" style={{ width: '100%' }} />
+                            <InputNumber placeholder="请输入" style={{ width: 'calc(100% - 24px)' }} />
                         </Form.Item>
 
                         <Form.Item label="可接受年利率上限" style={{ marginBottom: 0 }}>
@@ -34,7 +49,7 @@ export class index extends Component {
                         </Form.Item>
 
                         <Form.Item label="期望还款周期">
-                            <Select defaultValue="1">
+                            <Select defaultValue="1" style={{ width: 'calc(100% - 24px)' }}>
                                 <Option value="1">一个月</Option>
                                 <Option value="2">三个月</Option>
                                 <Option value="3">半年以上</Option>
@@ -42,24 +57,24 @@ export class index extends Component {
                         </Form.Item>
 
                         <Form.Item label="借款时间">
-                            <DatePicker style={{ width: '100%' }} />
+                            <DatePicker style={{ width: 'calc(100% - 24px)' }} />
                         </Form.Item>
 
                         <Form.Item label="联系方式">
-                            <Input placeholder="请输入" />
+                            <Input placeholder="请输入" style={{ width: 'calc(100% - 24px)' }}/>
                         </Form.Item>
 
-                        <Form.Item style={{ marginBottom: 0, textAlign: 'center'}}>
-                            <Button type="primary" htmlType="submit" >
+                        <Form.Item style={{marginLeft: '36%', textAlign: 'right'}}>
+                            <Button type="primary" htmlType="submit" onClick={this.props.handleSubmit}>
                                 提交
                             </Button>
                         </Form.Item>
 
-                  </Form>
-                
+                    </Form>
+                </div>
             </div>
         )
     }
 }
 
-export default index
+export default LoanInfo
