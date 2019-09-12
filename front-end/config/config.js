0a8608c406f62811c9911307243f0074e2af4077
luowebb <151220068@smail.nju.edu.cn>
2019-09-12 10:37:12 +0800

    clean GlobalHeader/RightContent

--- front-end/config/config.js
+++ front-end/config/config.js
@@ -102,7 +102,7 @@ export default {
     {
       path: '/',
       component: '../layouts/BasicLayout',
-      Routes: ['src/pages/Authorized'],
+      // Routes: ['src/pages/Authorized'],
       authority: ['admin', 'user'],
       routes: [
         {
