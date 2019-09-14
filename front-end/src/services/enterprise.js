79a6876f91d7780ba74000adceb72ad12587bfb8
Geqiao <171250029@smail.nju.edu.cn>
2019-09-14 20:09:52 +0800

    企业信息第一版

--- front-end/src/services/enterprise.js
+++ front-end/src/services/enterprise.js
@@ -20,3 +20,13 @@ export const getAllContract = async (user_name, user_type) => {
   const res = await request.post('/contract/getAll', {data: { user_name, user_type}});
   return res['content'];
 };
+
+export const getEntUserInfo = async (company_name) => {
+  const res = await request.get('/infoMan/entUserInfo', { data: { company_name }});
+  return res['content'];
+};
+
+export const changeEntUser = async (postData) =>{
+  const res = await request.post('/infoMan/changeEntUser',{data:postData});
+  return res['success'];
+};
